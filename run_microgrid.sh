0093e5a14335aeaa92fc2c488c273587dd0e7e69
Amin Hajjari <107117597+aminhajjari@users.noreply.github.com>
2026-06-08 09:30:36 +0330

    Update run_microgrid.sh

--- run_microgrid.sh
+++ run_microgrid.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=hma_drl_microgrid
 #SBATCH --account=def-arashmoh
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:a100:1
 #SBATCH --cpus-per-task=8
